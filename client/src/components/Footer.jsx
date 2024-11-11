21c206e7b12445e18b110f88dd8491a161315c9d
stevendev43 <herstevendev@gmail.com>
2024-11-11 13:41:54 -0300

    terminando el header y footer, agregando componente ByeChelloP

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,13 +7,13 @@ function Footer() {
     <footer className="bg-[#F5F5F5] text-[#333333] font-sans h-80">
       <div className="container mx-auto py-8 px-4">
         <div className="flex flex-col md:flex-row justify-between">
-          <div className="mb-4 md:mb-0 text-center">
+          <div className="md:mb-0 text-center">
             <Logo />
-            <p className='font-normal text-xs mt-[-10px]'>Buenos Aires - Argentina</p>
-            <p className='font-normal text-xs mt-[-2px]'>Derechos reservados | 2024</p>
+            <p className='font-normal text-xs mt-[-4px]'>Buenos Aires - Argentina</p>
+            <p className='font-normal text-xs'>Derechos reservados | 2024</p>
           </div>
           <div className="flex flex-col md:flex-row space-x-8">
-            <div>
+            <div className='text-right'>
               <h3 className="text-lg font-semibold font-sans pb-4">Enlaces rápidos</h3>
               <ul>
                 <li className='pb-2'><Link to='/Institucional' className='font-light hover:text-[#16325B] hover:font-normal'>Perfil</Link></li>
--- client/src/pages/inicio.jsx
+++ client/src/pages/inicio.jsx
@@ -1,14 +1,28 @@
-
+import ByeChelloP from '../components/ByeChelloP.jsx';
 
 
 function Inicio()  {
     return (
     <div>
-        <div className="flex">
-            <div className="h-screen w-full justify-center items-center mt-48">
-                <h1 className="text-6xl font-thin tracking-wider text-[#313131]">Disfrutá tu experiencia académica que nosotros te la organizamos.</h1>
+        <div className="grid grid-cols-2 row-span-2 pt-32 h-screen">
+            <div className="h-screen justify-center items-center">
+                <h1 className="text-5xl mt-20 font-sans font-thin tracking-wider text-[#313131]">Disfrutá tu experiencia académica que nosotros te la organizamos.</h1>
+                <button className="bg-[#313131] text-white tracking-wide font-sans font-medium py-3 px-5 rounded-2xl mt-20 text-2xl">Registrate</button>
+            </div>
+            <div className="grid grid-cols-2 gap-4 h-[600px]">
+                <div className="grid grid-cols-1 gap-4">
+                    <div className="bg-[#DFEE06] rounded-2xl"></div>
+                    <div className="bg-[#8FD14F] rounded-2xl"></div>
+                </div>
+                <div className="grid grid-cols-1">
+                    <div className="row-span-2 bg-[#9783FD] rounded-2xl"></div>
+                </div>
             </div>
         </div>
+        <div className="w-full  h-screen justify-center items-center flex"> 
+            <h2 className="text-4xl font-sans fonr-semibold text-[#313131]">Mindnote es tu compañero digital para organizar tus estudios  y tareas. Crea tableros personalizados, establece recordatorios y mantené todo bajo control. ¡Aumentá tu productividad y reducí el estrés!</h2>
+        </div>
+        <ByeChelloP />
     </div>
 )}
 
